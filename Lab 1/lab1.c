4440a1d70ebf47a2ad70efb5bf452b7bc8210b51
amp5646@yahoo.com <amp5646@yahoo.com>
2021-01-13 15:06:22 -0500

    Fix to pass IDE test

--- Lab 1/lab1.c
+++ Lab 1/lab1.c
@@ -3,9 +3,9 @@
 
 struct Student
 {
-    int student_id;
+    int student_ID;
     int g1, g2, g3;
-    float avg;
+    float average;
 };
 
 struct Student getMaxAverageStudent(struct Student *s, int n)
@@ -14,9 +14,9 @@ struct Student getMaxAverageStudent(struct Student *s, int n)
     float max = -1;
     for (int i = 0; i < n; i++)
     {
-        if (s[i].avg > max)
+        if (s[i].average > max)
         {
-            max = s[i].avg;
+            max = s[i].average;
             max_idx = i;
         }
     }
@@ -38,20 +38,20 @@ int main()
 
     for (int i = 0; i < n; i++)
     {
-        fscanf(infile, "%d", &students[i].student_id);
+        fscanf(infile, "%d", &students[i].student_ID);
         fscanf(infile, "%d %d %d", &students[i].g1, &students[i].g2, &students[i].g3);
-        students[i].avg = (students[i].g1 + students[i].g2 + students[i].g3) / 3.0;
+        students[i].average = (students[i].g1 + students[i].g2 + students[i].g3) / 3.0;
 
-        printf("%d %d %d %d %0.2f\n", students[i].student_id, students[i].g1, students[i].g2, students[i].g3, students[i].avg);
+        printf("%d %d %d %d %0.2f\n", students[i].student_ID, students[i].g1, students[i].g2, students[i].g3, students[i].average);
 
-        fprintf(outfile, "%d %d %d %d %0.2f\n", students[i].student_id, students[i].g1, students[i].g2, students[i].g3, students[i].avg);
+        fprintf(outfile, "%d %d %d %d %0.2f\n", students[i].student_ID, students[i].g1, students[i].g2, students[i].g3, students[i].average);
     }
 
     struct Student maxStudent = getMaxAverageStudent(students, n);
 
-    printf("\nMaximum Average is %0.2f and the student is %d\n", maxStudent.avg, maxStudent.student_id);
+    printf("\nMaximum Average is %0.2f and the student is %d\n", maxStudent.average, maxStudent.student_ID);
 
-    fprintf(outfile, "\nMaximum Average is %0.2f and the student is %d", maxStudent.avg, maxStudent.student_id);
+    fprintf(outfile, "\nMaximum Average is %0.2f and the student is %d\n", maxStudent.average, maxStudent.student_ID);
 
     fclose(infile);
     fclose(outfile);
